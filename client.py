b3a344cb56c198ce37548b16ad4b6782dee70a7c
shawnbmccarthy <sbmccarth@gmail.com>
2024-01-29 17:15:47 -0500

    added some client tests

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ from viam.components.sensor import Sensor
 from viam.logging import getLogger
 from viam.robot.client import RobotClient
 
-from emulators.simple_robot import ROBOT_TASK
+from emulators.simple_robot import RobotTask, CliffMode, ScanType
 
 logger: Logger = getLogger(__name__)
 
@@ -31,11 +31,12 @@ async def main() -> None:
         logger.info(f'resource name: {rn}')
 
     # TODO: create preset sensors ( all C params)
-    debug: Sensor = Sensor.from_robot(robot, 'debug')
+    preset: Sensor = Sensor.from_robot(robot, 'presets')
 
     components: List[Union[PowerSensor, Sensor]] = [
         Sensor.from_robot(robot, 'status'),
         Sensor.from_robot(robot, 'navigation'),
+        Sensor.from_robot(robot, 'debug'),
         PowerSensor.from_robot(robot, 'battery1'),
         PowerSensor.from_robot(robot, 'battery2')
     ]
@@ -45,12 +46,21 @@ async def main() -> None:
         reading = await component.get_readings()
         logger.info(f'reading: {reading}')
 
+
+
     # TODO: change presets
+    result = await preset.do_command(command={'SCAN_TYPE': ScanType.SINGLE.value})
+    logger.info(f'result: {result}')
+    result = await preset.do_command(command={'CLIFF_SENSOR_MODE': CliffMode.COMBINED.value})
+    logger.info(f'result: {result}')
+
+    reading = await preset.get_readings()
+    logger.info(f'reading: {reading}')
 
     # set tasks
-    for key in ROBOT_TASK.keys():
-        result = await debug.do_command(command={'ROBOT_TASK': int(key)})
-        logger.info(f'ROBOT_TASK[{ROBOT_TASK[key]}] - result: {result}')
+    for task in RobotTask:
+        result = await preset.do_command(command={'ROBOT_TASK': task.value})
+        logger.info(f'{task.name}- result: {result}')
         time.sleep(2)
 
     await robot.close()
